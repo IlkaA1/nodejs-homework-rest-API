861fed98a060384fa006d0b850d810a62a388da5
Ilona Kamash <ilonka.kamash@gmail.com>
2024-01-18 10:45:50 +0100

    chang and update

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const ctrl = require("../../controllers/contacts");
+
 const ctrl = require("../../controllers/contacts");
 
 const router = express.Router();
